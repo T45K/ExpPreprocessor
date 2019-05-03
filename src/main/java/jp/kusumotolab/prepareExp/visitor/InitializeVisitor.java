1577988565a3777790ce45ccb8f891116a1380b0
tsk-nkgw <tasktas9@gmail.com>
2019-05-03 19:11:56 +0900

    update

--- src/main/java/jp/kusumotolab/prepareExp/visitor/InitializeVisitor.java
+++ src/main/java/jp/kusumotolab/prepareExp/visitor/InitializeVisitor.java
@@ -1,23 +1,36 @@
 package jp.kusumotolab.prepareExp.visitor;
 
 import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
-import org.eclipse.jdt.core.dom.VariableDeclarationExpression;
+import org.eclipse.jdt.core.dom.Modifier;
+import org.eclipse.jdt.core.dom.PrimitiveType;
+import org.eclipse.jdt.core.dom.Type;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
 
 public class InitializeVisitor extends ASTVisitor {
     @Override
-    public boolean visit(final VariableDeclarationExpression node) {
-        return super.visit(node);
-    }
-
-    @Override
     public boolean visit(final VariableDeclarationStatement node) {
-        return super.visit(node);
-    }
+        for (final Object object : node.fragments()) {
+            final VariableDeclarationFragment fragment = (VariableDeclarationFragment) object;
+            if (fragment.getInitializer() == null) {
+                if(Modifier.isFinal(node.getModifiers())){
+                    node.modifiers().remove(Modifier.ModifierKeyword.FINAL_KEYWORD);
+                }
+            }
 
-    @Override
-    public boolean visit(final SingleVariableDeclaration node) {
-        return super.visit(node);
+            final Type type = node.getType();
+            if(type instanceof PrimitiveType){
+                if(type.toString().endsWith("boolean")){
+                    fragment.setInitializer(node.getAST().newBooleanLiteral(false));
+                }else {
+                    fragment.setInitializer((node.getAST().newNumberLiteral("0")));
+
+                }
+            }else{
+                fragment.setInitializer(node.getAST().newNullLiteral());
+            }
+        }
+
+        return true;
     }
 }
